5a96c050eeda78572ff416d91843a9103ed596f7
Chi Pham <chi.pham@aleph-labs.com>
2023-02-10 20:47:36 +0700

    Reserve a string
    Reserve a string

---
+++ interview/ReverseString.java
@@ -0,0 +1,14 @@
+package interview;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        String input = "avaj ni gnirts a esreveR";
+        StringBuffer output = new StringBuffer();
+
+        for(int index = input.length() - 1; index >= 0; index--) {
+            output.append(input.charAt(index));
+        }
+
+        System.out.println(output);
+    }
+}
